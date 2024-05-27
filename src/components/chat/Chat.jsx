23f1166c4879cb5361121f83048bb9502dbaa23d
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-27 14:22:27 -0400

    Update Chat.jsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,36 @@
 import React from "react";
 import "../chat/chat.css";
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="../avatar.png" alt="" />
+          <div className="texts">
+            <span>Utsav Patel</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="../phone.png" alt="" />
+          <img src="../video.png" alt="" />
+          <img src="../info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="../img.png" alt="" />
+          <img src="../camera.png" alt="" />
+          <img src="../mic.png" alt="" />
+        </div>
+        <input type="text" placeholder="Type a message" />
+        <div className="emoji">
+          <img src="../emoji.png" alt="" />
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 };
 export default Chat;
