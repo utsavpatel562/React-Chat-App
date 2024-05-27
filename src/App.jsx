ad1b8322973c0374e14841a256e2519ccd12f57a
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-27 12:33:39 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <List />
+        <Chat />
+        <Detail />
+      </div>
+    </>
+  );
+};
+
+export default App;
