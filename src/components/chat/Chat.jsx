639fef0b6b7887c074004673f42b1a0a3c05e028
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-29 14:09:29 -0400

    Update Chat.jsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
 import "../chat/chat.css";
 const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+    setOpen(false);
+  };
+  console.log(text);
+
   return (
     <div className="chat">
       <div className="top">
@@ -17,16 +26,99 @@ const Chat = () => {
           <img src="../info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="../avatar.png" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              voluptatem impedit incidunt temporibus explicabo ex praesentium,
+              ullam vitae consequatur facilis, officiis magnam aspernatur quod
+              fugit mollitia laudantium quasi dolor quam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              voluptatem impedit incidunt temporibus explicabo ex praesentium,
+              ullam vitae consequatur facilis, officiis magnam aspernatur quod
+              fugit mollitia laudantium quasi dolor quam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="../avatar.png" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              voluptatem impedit incidunt temporibus explicabo ex praesentium,
+              ullam vitae consequatur facilis, officiis magnam aspernatur quod
+              fugit mollitia laudantium quasi dolor quam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              voluptatem impedit incidunt temporibus explicabo ex praesentium,
+              ullam vitae consequatur facilis, officiis magnam aspernatur quod
+              fugit mollitia laudantium quasi dolor quam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="../avatar.png" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              voluptatem impedit incidunt temporibus explicabo ex praesentium,
+              ullam vitae consequatur facilis, officiis magnam aspernatur quod
+              fugit mollitia laudantium quasi dolor quam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="../profile2.jpg" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
+              voluptatem impedit incidunt temporibus explicabo ex praesentium,
+              ullam vitae consequatur facilis, officiis magnam aspernatur quod
+              fugit mollitia laudantium quasi dolor quam!
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <img src="../img.png" alt="" />
           <img src="../camera.png" alt="" />
           <img src="../mic.png" alt="" />
         </div>
-        <input type="text" placeholder="Type a message" />
+        <input
+          type="text"
+          placeholder="Type a message"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <div className="emoji">
-          <img src="../emoji.png" alt="" />
+          <img
+            src="../emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
         </div>
         <button className="sendButton">Send</button>
       </div>
