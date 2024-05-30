3e90d98b39ef96b57d8c048906356f0c935b120a
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-30 10:34:24 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import "./App.css";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
+import Login from "./components/login/login";
 
 const App = () => {
+  const user = false;
+
   return (
-    <>
-      <div className="container">
-        <List />
-        <Chat />
-        <Detail />
-      </div>
-    </>
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
   );
 };
 
