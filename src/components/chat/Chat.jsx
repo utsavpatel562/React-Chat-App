32bed5444670d0e3635853f5e1f41f2ba38d9939
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-30 10:33:58 -0400

    Update Chat.jsx

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import EmojiPicker from "emoji-picker-react";
 import "../chat/chat.css";
 const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
+
+  const endRef = useRef(null);
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
@@ -97,6 +103,7 @@ const Chat = () => {
             <span>1 min ago</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
       <div className="bottom">
         <div className="icons">
