825d90a49cc3e9ff64f030b13f601c02240d5b3e
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-31 10:58:30 -0400

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "react-chatapp-d5410.firebaseapp.com",
-  projectId: "react-chatapp-d5410",
-  storageBucket: "react-chatapp-d5410.appspot.com",
-  messagingSenderId: "243413298033",
-  appId: "1:243413298033:web:6e09d5a983bed0fa4aa4d0"
+  REACT_APP_FIREBASE_API_KEY=your_api_key
+  REACT_APP_FIREBASE_AUTH_DOMAIN=your_auth_domain
+  REACT_APP_FIREBASE_PROJECT_ID=your_project_id
+  REACT_APP_FIREBASE_STORAGE_BUCKET=your_storage_bucket
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID=your_messaging_sender_id
+  REACT_APP_FIREBASE_APP_ID=your_app_id
 };
 
 // Initialize Firebase
