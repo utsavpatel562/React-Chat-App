e6ce928d562294cbb0e9fd330aa9a5abc7f96f8b
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-05-31 11:01:43 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/login";
+import Notification from "./components/notification/notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
   const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <div className="container">
@@ -18,6 +31,7 @@ const App = () => {
       ) : (
         <Login />
       )}
+      <Notification />
     </div>
   );
 };
