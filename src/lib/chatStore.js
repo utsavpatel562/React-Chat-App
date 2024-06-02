b4c1296e45042571781194140ce3c1c2a536201a
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-01 21:16:08 -0400

    Add files via upload

---
+++ src/lib/chatStore.js
@@ -0,0 +1,40 @@
+import { create } from "zustand";
+import { useUserStore } from "./userStore";
+
+export const useChatStore = create((set) => ({
+  chatId: null,
+  user: null,
+  isCurrentUserBlocked: false,
+  isRecieverBlocked: false,
+  changeChat: (chatId, user) =>{
+    const currentUser = useUserStore.getState().currentUser;
+    // CHECK IF CURRENT USER IS BLOCKED
+    if(user.blocked.includes(currentUser.id)){
+        return set({
+            chatId,
+            user: null,
+            isCurrentUserBlocked: true,
+            isRecieverBlocked: false,
+        })
+    }
+    // CHECK IF RECIVIER IS BLOCKED
+    else if(currentUser.blocked.includes(user.id)){
+        return set({
+            chatId,
+            user: user,
+            isCurrentUserBlocked: false,
+            isRecieverBlocked: true,
+        })
+    }  else {
+    return set({
+        chatId,
+        user,
+        isCurrentUserBlocked: false,
+        isRecieverBlocked: false,
+    })
+}
+  },  
+  changeBlock: () =>{
+    set(state=>({...state, isRecieverBlocked: !state.isRecieverBlocked}))
+  }
+}));
