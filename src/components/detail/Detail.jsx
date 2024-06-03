3509a1d35adc6022bd1fbbc78fe82479f2b0cea5
Utsav Patel ✨ <158522755+utsavpatel562@users.noreply.github.com>
2024-06-03 18:18:11 -0400

    Update Detail.jsx

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "../detail/detail.css";
-import { auth } from "../../lib/firebase";
+import { auth, db } from "../../lib/firebase";
+import { useChatStore } from "../../lib/chatStore";
+import { useUserStore } from "../../lib/userStore";
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Detail = () => {
+  const { chatId, user, isCurrentUserBlocked, isRecieverBlocked, changeBlock } =
+    useChatStore();
+
+  const { currentUser } = useUserStore();
+  const handleBlock = async () => {
+    if (!user) return;
+    const userDocRef = doc(db, "users", currentUser.id);
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isRecieverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+      });
+      changeBlock();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
       <div className="detail">
         <div className="user">
-          <img src="../avatar.png" alt="" />
-          <h3>Utsav Patel</h3>
+          <img src={user?.avatar || "../avatar.png"} alt="" />
+          <h3>{user?.username}</h3>
           <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <div className="info">
@@ -52,7 +71,13 @@ const Detail = () => {
               <img src="../arrowUp.png" alt="" />
             </div>
           </div>
-          <button>Block User</button>
+          <button onClick={handleBlock}>
+            {isCurrentUserBlocked
+              ? "You are Blocked!"
+              : isRecieverBlocked
+              ? "User Blocked"
+              : "Block User"}
+          </button>
           <button className="logout" onClick={() => auth.signOut()}>
             Logout
           </button>
